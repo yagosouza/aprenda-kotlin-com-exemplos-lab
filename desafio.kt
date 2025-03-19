2701e22eea86068c94de1ad5443985d40b1bac84
Yago Souza <yaago.souza@hotmail.com>
2025-03-19 09:03:38 -0300

    commit refactor desafio kotlin

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,40 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60, val nivel: Nivel)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
+    private val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        if (!inscritos.contains(usuario)) {
+            inscritos.add(usuario)
+            println("Usuário ${usuario.nome} matriculado na formação $nome.")
+        } else {
+            println("Usuário ${usuario.nome} já está matriculado na formação $nome.")
+        }
+    }
+    
+    fun listarInscritos() {
+        println("Inscritos na formação $nome:")
+        inscritos.forEach { println(it.nome) }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("Ana")
+    val usuario2 = Usuario("Carlos")
+    
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 90, Nivel.BASICO)
+    val conteudo2 = ConteudoEducacional("Programação Funcional em Kotlin", 120, Nivel.INTERMEDIARIO)
+    
+    val formacao = Formacao("Desenvolvimento Kotlin", listOf(conteudo1, conteudo2))
+    
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+    formacao.matricular(usuario1) // Testando re-matrícula
+    
+    formacao.listarInscritos()
 }
